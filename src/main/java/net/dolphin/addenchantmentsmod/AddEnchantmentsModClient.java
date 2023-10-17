c180b8ddc260435314e50d16c3b8b9f023a4cba8
amitaiflint <amitaiflint@gmail.com>
2023-10-17 16:53:25 +0300

    Initial commit

---
+++ src/main/java/net/dolphin/addenchantmentsmod/AddEnchantmentsModClient.java
@@ -0,0 +1,10 @@
+package net.dolphin.addenchantmentsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AddEnchantmentsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
